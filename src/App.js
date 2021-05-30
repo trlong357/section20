292761d4e4425fb7ae6bc809671b012b6d67038f
Long Tran <trlong357@gmail.com>
2021-05-30 15:30:27 +0700

    268. Using NavLink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route } from "react-router-dom";
-import Welcome from "./components/Welcome";
-import Products from "./components/Products";
+import Welcome from "./components/pages/Welcome";
+import Products from "./components/pages/Products";
+import MainHeader from "./components/MainHeader";
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
