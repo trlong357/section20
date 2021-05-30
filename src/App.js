26541eb9137fc18844a301bf8c1df02a2dc2f357
Long Tran <trlong357@gmail.com>
2021-05-30 14:59:00 +0700

    267. Defining & USing Router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
